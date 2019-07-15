8365763992fcbdd0e92711187f900772ad373d11
MrLinZHui <742847492@qq.com>
2019-07-15 19:57:59 +0800

    rename

--- src/main/java/com/thoughtworks/tdd/Story6/AC1/SmartParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/Story6/AC1/SmartParkingBoy.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 
 public class SmartParkingBoy extends ParkingBoy {
     @Override
-    public Ticket park(ArrayList<ParkingLot> arrayList, Car car) {
-      Optional <ParkingLot> parkingLot = arrayList.stream().reduce((parkinglot1, parkinglot2) ->
+    public Ticket park(ArrayList<ParkingLot> parkingLotsarrayList, Car car) {
+      Optional <ParkingLot> parkingLot = parkingLotsarrayList.stream().reduce((parkinglot1, parkinglot2) ->
               (parkinglot1.getCapacity() -parkinglot1.getActualcapacity())>(parkinglot2.getCapacity()-parkinglot2.getActualcapacity())?parkinglot1:parkinglot2);
       ParkingLot parkingLot1 =  parkingLot.get();
       System.out.print(String.format(parkingLot1.getParkingLotName(),"\n"));
